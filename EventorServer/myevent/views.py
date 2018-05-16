c788e95ae1411b53984849a215c46ee2ee534e0d
alirezafarahabadi <aliabadi7697@gmail.com>
2018-05-16 10:06:42 +0430

    create urls

--- EventorServer/myevent/views.py
+++ EventorServer/myevent/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+
+
+class HelloWorld(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({
+            "mes":"sfasfsdf",
+        })
